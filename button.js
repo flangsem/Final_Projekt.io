3f1a75daad8aa1b6d3203af97cd5f28bb310c36f
flangsem <flangsem@gmail.com>
2020-12-01 23:30:17 +0100

    Delete button.js

--- button.js
+++
@@ -1,7 +0,0 @@
-var buttonUp = document.querySelector("#btnlogo");
-
-function scrollUp() {
-	var targetPosition = 0;
-	var startPosition = window.pageYOffset;
-	var distance = targetPosition - startPosition;
-	let scroll = null;
