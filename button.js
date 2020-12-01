3665bdbfb179e06abf352cc09f79f94dda961120
flangsem <flangsem@gmail.com>
2020-12-01 12:19:16 +0100

    Create button.js

---
+++ button.js
@@ -0,0 +1,7 @@
+var buttonUp = document.querySelector("#btnlogo");
+
+function scrollUp() {
+	var targetPosition = 0;
+	var startPosition = window.pageYOffset;
+	var distance = targetPosition - startPosition;
+	let scroll = null;
